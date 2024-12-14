e0b41a8851e8d24183cce09b73e21eb413b71a44
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2024-12-14 12:27:22 +0700

    Time: 0 ms (100%), Space: 18.4 MB (97.88%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.go
@@ -0,0 +1,23 @@
+func increasingTriplet(nums []int) bool {
+    first := nums[0]
+    currFirst := first
+
+    second := -1
+    for i:= 1; i < len(nums); i++ {
+        third := nums[i]
+        if third < currFirst {
+            currFirst = third
+        } else if third > currFirst {
+            if second == -1{
+                second = third
+            } else {
+                if third > second {
+                    return true
+                } else {
+                    second = third
+                }
+            }            
+        }
+    }
+    return false
+}
