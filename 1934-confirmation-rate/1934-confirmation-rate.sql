e896a8a8b163cbc5d872766f5a91ae1433dceba3
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2024-12-25 08:17:51 +0700

    Time: 997 ms (21.06%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,13 +1,14 @@
 # Write your MySQL query statement below
-SELECT S.user_id, ROUND(COALESCE(total_confirmed/COUNT(S.user_id), 0),2) AS confirmation_rate
-FROM Signups AS S
-LEFT JOIN Confirmations AS C
-ON S.user_id = C.user_id
-LEFT JOIN (
-    SELECT CT.user_id, Count(CT.action) AS total_confirmed
-    FROM Confirmations AS CT
-    WHERE CT.action = "confirmed"
-    GROUP BY CT.user_id
-) As CT
-ON CT.user_id = S.user_id
-GROUP BY S.user_id
+SELECT 
+    S.user_id,
+    ROUND(
+        COALESCE(SUM(C.action = 'confirmed') / COUNT(C.user_id), 0)
+    , 2) AS confirmation_rate
+FROM 
+    Signups AS S
+LEFT JOIN 
+    Confirmations AS C
+ON 
+    S.user_id = C.user_id
+GROUP BY 
+    S.user_id
