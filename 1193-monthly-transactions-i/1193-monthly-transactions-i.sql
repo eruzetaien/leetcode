3bee79a27c47294dae8f9d3bdca922edd29a8a0e
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-01-01 00:13:25 +0700

    Time: 988 ms (23.46%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,26 +1,12 @@
 # Write your MySQL query statement below
 SELECT 
-    DATE_FORMAT(T.trans_date, '%Y-%m') AS month, 
-    T.country, 
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
     COUNT(*) AS trans_count,
-    COUNT(TA.trans_date) AS approved_count,
-    COALESCE(SUM(T.amount),0) AS trans_total_amount,
-    COALESCE(SUM(TA.amount),0) AS approved_total_amount
+    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
 FROM 
-    Transactions AS T
-LEFT JOIN 
-    (
-        SELECT
-            trans_date,
-            country,
-            amount
-        FROM 
-            Transactions 
-        WHERE state = 'approved'
-    ) AS TA
-ON T.trans_date = TA.trans_date AND 
-    COALESCE(T.country, 'Unknown') = COALESCE(TA.country, 'Unknown') AND 
-    TA.amount = T.amount
+    Transactions
 GROUP BY 
-    T.country, 
-    DATE_FORMAT(T.trans_date, '%Y-%m')
+    month, country;
