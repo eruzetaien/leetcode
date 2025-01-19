6c54c537d90ef5cdbab91cd034f3b9824dcb487a
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-01-19 07:59:28 +0700

    Time: 559 ms (41.68%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,19 +1,10 @@
 # Write your MySQL query statement below
-WITH OddPositionNumbers AS (
-    SELECT student, ROW_NUMBER() OVER (ORDER BY id) AS row_num
-    FROM Seat
-    WHERE id % 2 = 1
-),
-EvenPositionNumbers AS (
-    SELECT student, ROW_NUMBER() OVER (ORDER BY id) AS row_num
-    FROM Seat
-    WHERE id % 2 = 0
-),
-Combined AS (
-    SELECT row_num, student, 'Table2' AS source FROM OddPositionNumbers
-    UNION ALL
-    SELECT row_num, student, 'Table1' AS source FROM EvenPositionNumbers
-)
-SELECT ROW_NUMBER() OVER (ORDER BY row_num, source) AS id,  student
-FROM Combined
-ORDER BY row_num, source;
+SELECT 
+    CASE 
+        WHEN id % 2 = 1 AND id + 1 <= (SELECT MAX(id) FROM Seat) THEN id + 1
+        WHEN id % 2 = 0 THEN id - 1
+        ELSE id
+    END AS id,
+    student
+FROM Seat
+ORDER BY id ASC;
