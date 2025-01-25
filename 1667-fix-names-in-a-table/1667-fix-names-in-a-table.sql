b407396f4641f769eee7f83f478c1d10915c2477
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-01-25 09:46:55 +0700

    Time: 1131 ms (39.68%), Space: 0B (100%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -3,7 +3,7 @@ SELECT
     user_id,
     CONCAT(
         UPPER(SUBSTRING(name, 1, 1)),
-        LOWER(SUBSTRING(name, 2, LENGTH(name)))
+        LOWER(SUBSTRING(name, 2))
     ) AS name
 FROM Users
 ORDER BY user_id
