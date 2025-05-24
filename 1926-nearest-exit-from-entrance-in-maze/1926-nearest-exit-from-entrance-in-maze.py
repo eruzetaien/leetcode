9b685c753f7b0fe51261526e121aac6aa6c64db9
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-05-25 06:19:56 +0700

    Time: 57 ms (78.64%), Space: 19.2 MB (78.07%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -1,24 +1,33 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
         m, n = len(maze), len(maze[0])
-        queue = deque()
-        queue.append((entrance[0], entrance[1], 0))
-        directions = [(-1,0), (1,0), (0,-1), (0,1)]
         
-        # Mark entrance as visited
-        maze[entrance[0]][entrance[1]] = '+'
+        start_row, start_col = entrance[0], entrance[1]
+        queue = [(start_row, start_col, 0)]
+        maze[start_row][start_col] = '+'
         
-        while queue:
-            r, c, steps = queue.popleft()
-            
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                
-                if 0 <= nr < m and 0 <= nc < n and maze[nr][nc] == '.':
-                    # Check if it's a border and not the entrance
-                    if nr == 0 or nr == m-1 or nc == 0 or nc == n-1:
-                        return steps + 1
-                    maze[nr][nc] = '+'
-                    queue.append((nr, nc, steps + 1))
+        total_step = -1
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        while (queue):
+            row, col, step = queue.pop(0)
+            is_in_border = row == 0 or col == 0 or row == m-1 or col == n-1
+            is_in_entrance = row == entrance[0] and col == entrance[1]
+
+            if (is_in_border and not is_in_entrance):
+                total_step = step
+                break
+
+            for direction in directions:
+                new_row = row + direction[0]
+                new_col = col + direction[1]
+
+                if (new_row >= m or new_col >= n or new_row < 0 or new_col < 0):
+                    continue
+
+                if (maze[new_row][new_col] == '.'):
+                    queue.append((new_row,new_col, step+1))
+                    maze[new_row][new_col] = '+' # visited
+
+        return total_step
         
-        return -1
+        
