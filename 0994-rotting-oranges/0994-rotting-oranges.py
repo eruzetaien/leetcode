d207513e398b916d9f61d46b643f2b7fe29cf51e
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-05-27 11:01:59 +0700

    Time: 3 ms (82.23%), Space: 18 MB (26.97%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -14,22 +14,19 @@ class Solution:
                     rotten.append((r, c))
                 elif grid[r][c] == 1:
                     fresh += 1
-        
         minutes = 0
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        
-        # Perform BFS
-        while rotten and fresh > 0:
+        directions = [(1,0), (0,1), (-1,0), (0,-1)]
+        while (rotten and fresh > 0):
             minutes += 1
-            # Process all current rotten oranges in this level
-            for _ in range(len(rotten)):
-                x, y = rotten.popleft()
-                for dx, dy in directions:
-                    nx, ny = x + dx, y + dy
-                    if 0 <= nx < rows and 0 <= ny < cols:
-                        if grid[nx][ny] == 1:
-                            grid[nx][ny] = 2
+            rotten_num = len(rotten)
+            for i in range(rotten_num):
+                row, col = rotten.popleft()
+                for dir in directions:
+                    new_row, new_col = row + dir[0], col + dir[1]
+                    if (0 <= new_row < rows and 0 <= new_col < cols ):
+                        if (grid[new_row][new_col] == 1):
+                            grid[new_row][new_col] = 2
+                            rotten.append((new_row, new_col))
                             fresh -= 1
-                            rotten.append((nx, ny))
-        
-        return minutes if fresh == 0 else -1
+
+        return minutes if fresh <= 0 else -1
