2b54bc53586051a5bb6c0085e4ad540c77a5a9dc
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-07-21 13:16:35 +0700

    Time: 4 ms (99.27%), Space: 55.1 MB (71.19%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -5,9 +5,12 @@ class Solution {
         int hold = prices[0];
 
         for (int i = 1; i < n; i++) {
-            int prevCash = cash;
             cash = Math.max(cash, prices[i] - hold - fee);
-            hold = Math.min(hold, prices[i] - prevCash);
+            hold = Math.min(hold, prices[i] - cash);
+            // if cash updated (cash = prices[i] - hold - fee), 
+            // we compare hold with prices[i] - (prices[i] - hold - fee)
+            // hold vs (hold + fee), so hold will not be updated, 
+            // this prevent we buy and sell at the same time 
         }
 
         return cash;
