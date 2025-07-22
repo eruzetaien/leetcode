11eb0ba7296f52adaea5da8a408e33f818b68722
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-07-22 10:09:45 +0700

    Time: 0 ms (100%), Space: 42 MB (89.68%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,23 +1,27 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (m == 0){
+            for (int i = 0; i < n; i++){nums1[i] = nums2[i];}
+            return;
+        }
+        if (n == 0){return;}
+
+        int insertPointer = m + n - 1;  
         int pointer1 = m-1;
         int pointer2 = n-1;
-        int insertPointer = m+n-1;
 
-        while (pointer2 >= 0 && pointer1 >=0) {
-            int num1 = nums1[pointer1];
-            int num2 = nums2[pointer2];
-            if (num1 >= num2){
-                nums1[insertPointer] = num1;
-                pointer1--;
-            } else {
-                nums1[insertPointer] = num2;
+        while (pointer1 >= 0 && pointer2 >= 0){
+            if (nums2[pointer2] > nums1[pointer1]){
+                nums1[insertPointer] = nums2[pointer2];
                 pointer2--;
+            } else {
+                nums1[insertPointer] = nums1[pointer1];
+                pointer1--;
             }
             insertPointer--;
         }
 
-        while (pointer2 >= 0) {
+        while (pointer2 >=  0){
             nums1[insertPointer] = nums2[pointer2];
             pointer2--;
             insertPointer--;
