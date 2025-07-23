27199ca6ee9f58eb32f07e669b2688505db83b07
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-07-23 10:02:45 +0700

    Time: 0 ms (100%), Space: 41.7 MB (64.6%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -11,7 +11,7 @@ class Solution {
 
         for (int i = 1; i < m; i++){
             // System.out.println(Arrays.toString(dp));
-            if (obstacleGrid[i][0] == 1){ dp[0] = 0}
+            if (obstacleGrid[i][0] == 1){ dp[0] = 0;}
             for (int j = 1; j < n; j++){
                 if (obstacleGrid[i][j]== 1){
                     dp[j] = 0;
