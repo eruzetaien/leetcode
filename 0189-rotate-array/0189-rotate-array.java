01bcc93430723fe2cf42062ce35a9e36bfed5dc7
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-07-23 12:41:42 +0700

    Time: 2 ms (25.74%), Space: 58.2 MB (7.69%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,21 +1,30 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        k = k % n; 
-        int count = 0;
-        
-        for (int start = 0; count < n; start++) {
-            int current = start;
-            int prev = nums[start];
+        k = k % n;
+        int numOfIterations = gcd(n,k);
 
+        for (int i = 0; i < numOfIterations; i++){
+            int startIdx = i;
+            int pointer = startIdx;
+            int temp = nums[pointer];
             do {
-                int next = (current + k) % n;
-                int temp = nums[next];
-                nums[next] = prev;
-                prev = temp;
-                current = next;
-                count++;
-            } while (start != current); 
+                int num = temp;
+                int newIdx = (pointer + k) % n;
+                temp = nums[newIdx];
+                nums[newIdx] = num;
+
+                pointer = newIdx;
+            } while (pointer != startIdx);
         }
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }
