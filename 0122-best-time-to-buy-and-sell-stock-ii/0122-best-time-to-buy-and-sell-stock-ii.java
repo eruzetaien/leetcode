8b4d4d768d0421bb04f9265603b7de8e7501b355
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-07-23 14:40:12 +0700

    Time: 0 ms (100%), Space: 45.7 MB (85.26%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,34 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices.length <=1){
-            return 0;
+        int n = prices.length;
+        int cash = 0;
+        int hold = prices[0];
+
+        for (int i = 1; i < n; i++){
+            cash = Math.max(cash, prices[i] - hold);
+            hold = Math.min(hold, prices[i] - cash);
         }
 
-        int buyPrice = prices[0];
-        int currentProfit = 0;
-        int currentSellPrice= 0;
-        int pointer= 1;
-
-        int totalProfit = 0;
-
-        while (pointer < prices.length){
-            int currentPrice = prices[pointer];
-            int profit = currentPrice  - buyPrice;
-
-            if (profit < 0 && currentSellPrice == 0) {
-                buyPrice = currentPrice;
-            } else if (profit > currentProfit){
-                currentProfit = profit;
-                currentSellPrice = currentPrice;
-            } else { // profit <= currentProfit
-                totalProfit += currentProfit;
-                currentProfit =0;
-                currentSellPrice=0;
-                buyPrice = currentPrice;
-            }
-            pointer++;
-        }
-        totalProfit += currentProfit;
-        return totalProfit;
+        return cash;
     }
 }
