b213c350c8b3c9b698e13d5bd57fc336dc19cee8
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-07-24 16:20:49 +0700

    Time: 0 ms (100%), Space: 41.2 MB (98.46%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,18 +1,24 @@
 class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
-        Integer[] sortedCitations = Arrays.stream(citations).boxed().toArray(Integer[]::new);
-        Arrays.sort(sortedCitations, Collections.reverseOrder());
-
-        int i = 0;
-        while (sortedCitations[i] >= i + 1 && i < n){
-            System.out.println(i);
-            System.out.println(sortedCitations[i]);
-            System.out.println();
-            i++;
-
+        int[] count = new int[n + 1]; 
+        
+        for (int c : citations) {
+            if (c >= n) {
+                count[n]++; // Jika lebih dari n, masukkan ke count[n]
+            } else {
+                count[c]++;
+            }
         }
-
-        return i;
+        
+        int h = 0;
+        for (int i = n; i >= 0; i--) {
+            h += count[i]; // Akumulasi jumlah paper
+            if (h >= i) {
+                return i; // H-index ditemukan
+            }
+        }
+        
+        return 0;
     }
 }
