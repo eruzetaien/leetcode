ad8f3cff4fc2b0e7737ca53e2b2af2fae230ec72
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-07-26 09:44:08 +0700

    Time: 2 ms (87.54%), Space: 55.3 MB (74.65%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,24 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        
-        Map<Integer, Integer> cumProduct = new HashMap<Integer, Integer>();
-        
-        int product = 1;
-        for (int i = 0; i<n; i++){
-            product *= nums[i];
-            cumProduct.put(i, product);
-        }
-
         int[] result = new int[n];
-        result[n-1] = cumProduct.get(n-2);
+        
+        // Cumulative Product with offset -1
+        result[0] = 1;
+        for (int i = 1; i<n; i++){
+            result[i] = result[i-1] * nums[i-1];
+        }
 
-        product = nums[n-1];        
-        for (int i = n-2; i>0; i--){
-            result[i] = cumProduct.get(i-1) * product;
+        // Reverse Cumulative Product with offset -1
+        int product = 1;
+        for (int i = n-1; i>=0; i--){
+            result[i] *= product;
             product *= nums[i];
         }
-        result[0] = product;
 
         return result;
     }
