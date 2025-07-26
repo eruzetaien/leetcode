ce4e3411adae6b48576a16ddf66273afd1c0b5b6
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-07-26 09:23:01 +0700

    Time: 12 ms (3.47%), Space: 61.7 MB (5.9%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,7 +3,6 @@ class Solution {
         int n = nums.length;
         
         Map<Integer, Integer> cumProduct = new HashMap<Integer, Integer>();
-        Map<Integer, Integer> reverseCumProduct = new HashMap<Integer, Integer>();
         
         int product = 1;
         for (int i = 0; i<n; i++){
@@ -11,19 +10,15 @@ class Solution {
             cumProduct.put(i, product);
         }
 
-        product = 1;
-        for (int i = n-1; i>0; i--){
-            product *= nums[i];
-            reverseCumProduct.put(i, product);
-        }
-        
         int[] result = new int[n];
-        result[0] = reverseCumProduct.get(1);
         result[n-1] = cumProduct.get(n-2);
 
-        for (int i = 1; i<n-1; i++){
-            result[i] = cumProduct.get(i-1) * reverseCumProduct.get(i+1);
+        product = nums[n-1];        
+        for (int i = n-2; i>0; i--){
+            result[i] = cumProduct.get(i-1) * product;
+            product *= nums[i];
         }
+        result[0] = product;
 
         return result;
     }
