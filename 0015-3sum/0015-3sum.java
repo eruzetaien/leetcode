c11dee7b6c9d6e4ec0a9ab9ff1e7ff72e9db6936
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-07-30 11:32:22 +0700

    Time: 33 ms (33.8%), Space: 51.2 MB (92.59%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,9 +3,10 @@ class Solution {
         int n = nums.length;
         Arrays.sort(nums);
 
-        Set<List<Integer>> output = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
         for (int i = 0; i < n -2; i++){
-            
+            if (i > 0 && nums[i] == nums[i-1]){continue;}
+
             int midPointer = i + 1;
             int rightPointer = n - 1;
 
@@ -13,6 +14,12 @@ class Solution {
                 int sum = nums[i] + nums[midPointer] + nums[rightPointer];
                 if (sum == 0){
                     output.add(Arrays.asList(nums[i], nums[midPointer], nums[rightPointer]));
+
+                    // Skip duplicate nums[midPointer]
+                    while (midPointer < rightPointer && nums[midPointer] == nums[midPointer + 1]) midPointer++;
+                    // Skip duplicate nums[rightPointer]
+                    while (midPointer < rightPointer && nums[rightPointer] == nums[rightPointer - 1]) rightPointer--;
+
                     midPointer++;
                     rightPointer--;
                 } else if (sum < 0){
@@ -23,6 +30,6 @@ class Solution {
             }
         }
 
-        return new ArrayList<>(output);
+        return output;
     }
 }
