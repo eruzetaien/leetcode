55a50dee02eca78ac375f74a6d90351ea697b780
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-08-03 11:36:05 +0700

    Time: 1 ms (72.4%), Space: 46 MB (15.79%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,23 +3,43 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        Set<Integer> zeroRows = new HashSet<>();
-        Set<Integer> zeroCols = new HashSet<>();
-        
+        boolean zeroInFirstRow = false, zeroInFirstCol = false;
+
+        for (int i = 0; i < m; i++){
+            if (matrix[i][0] == 0){zeroInFirstCol = true;}
+        }
+
+        for (int j = 0; j < n; j++){
+            if (matrix[0][j] == 0){zeroInFirstRow = true;}
+        }
+
         for (int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
                 if (matrix[i][j] == 0){
-                    zeroRows.add(i);
-                    zeroCols.add(j);
+                    matrix[i][0] = 0; // Mark zero in first col
+                    matrix[0][j] = 0; // Mark zero in first row
                 }
             }
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (zeroRows.contains(i) || zeroCols.contains(j)) {
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+
+        if (zeroInFirstCol){
+            for (int i = 0; i < m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (zeroInFirstRow){
+            for (int j = 0; j < n; j++){
+                matrix[0][j] = 0;
             }
         }
     }
