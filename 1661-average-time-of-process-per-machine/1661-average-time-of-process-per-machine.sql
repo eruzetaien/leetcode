b19790282292e935519bf324d9ead27832565560
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-08-05 13:02:28 +0700

    Time: 165 ms (82.36%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,8 +1,8 @@
-# Write your MySQL query statement below
-SELECT p_table.machine_id, ROUND(AVG(ABS(p_table.timestamp - c_table.timestamp)),3) AS processing_time
-FROM Activity as p_table 
-JOIN Activity as c_table
-ON p_table.machine_id = c_table.machine_id AND
-    p_table.process_id = c_table.process_id AND
-    p_table.activity_type != c_table.activity_type
-GROUP BY p_table.machine_id;
+-- Write your PostgreSQL query statement below
+SELECT a1.machine_id, ROUND(AVG(ABS(a2.timestamp-a1.timestamp))::numeric,3) AS processing_time
+FROM Activity AS a1
+JOIN Activity AS a2
+ON a1.machine_id = a2.machine_id AND 
+    a1.process_id = a2.process_id AND
+    a1.activity_type != a2.activity_type
+GROUP BY a1.machine_id
