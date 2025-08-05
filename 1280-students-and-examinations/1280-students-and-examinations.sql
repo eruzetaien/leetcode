856b7d97ed068221f0791df67d0dbc4f4c2250c7
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-08-05 13:28:33 +0700

    Time: 483 ms (19.27%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,13 +1,12 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT 
-    Std.student_id, 
-    Std.student_name, 
-    Sbj.subject_name, 
-    COALESCE(COUNT(Exm.subject_name), 0) AS attended_exams
-FROM Students AS Std
-CROSS JOIN Subjects AS Sbj  -- Ensure all subjects for each student
-LEFT JOIN Examinations AS Exm
-    ON Std.student_id = Exm.student_id
-    AND Sbj.subject_name = Exm.subject_name
-GROUP BY Std.student_id, Std.student_name, Sbj.subject_name
-ORDER BY Std.student_id, Std.student_name, Sbj.subject_name;
+    s.student_id,
+    s.student_name,
+    sub.subject_name,
+    COUNT(e.subject_name) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e
+    ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, sub.subject_name
+ORDER BY s.student_id, sub.subject_name;
