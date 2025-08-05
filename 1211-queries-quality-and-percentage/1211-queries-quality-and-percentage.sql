d42e9f01f06f286577c1bbf49bb8ab91421dab75
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-08-05 15:29:20 +0700

    Time: 217 ms (91.38%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,16 +1,7 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT 
-    Q.query_name,
-    ROUND(AVG(Q.rating/Q.position),2) AS quality,
-    COALESCE(ROUND(LR.low_rating/COUNT(Q.query_name) * 100.0,2),0) AS poor_query_percentage
-FROM 
-    Queries AS Q
-LEFT JOIN 
-    (
-        SELECT DISTINCT query_name, COUNT(rating) AS low_rating
-        FROM Queries
-        WHERE rating < 3
-        GROUP BY query_name
-    ) AS LR
-ON LR.query_name = Q.query_name
-GROUP BY Q.query_name
+    query_name, 
+    ROUND(AVG(rating * 1.0/position), 2) AS quality, 
+    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0 /COUNT(*), 2) AS poor_query_percentage
+FROM queries 
+GROUP BY query_name
