9f0b98ae81b99086b5522ac155216b73583246ff
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-08-06 10:13:45 +0700

    Time: 340 ms (90.33%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,41 +1,20 @@
 -- Write your PostgreSQL query statement below
-WITH recent_changes AS (
-    SELECT 
-        product_id,
-        new_price,
-        change_date
-    FROM 
-        Products
-    WHERE 
-        change_date <= '2019-08-16'
+WITH AllProducts AS (
+    SELECT DISTINCT product_id
+    FROM Products
 ),
-latest_change AS (
-    SELECT 
-        product_id,
-        MAX(change_date) AS latest_date
-    FROM 
-        recent_changes
-    GROUP BY 
-        product_id
-),
-prices_on_date AS (
-    SELECT 
-        rc.product_id,
-        rc.new_price
-    FROM 
-        recent_changes rc
-    JOIN 
-        latest_change lc
-    ON 
-        rc.product_id = lc.product_id 
-        AND rc.change_date = lc.latest_date
+ProductLastUpdate AS (
+    SELECT product_id, MAX(change_date) AS last_change_date
+    FROM Products
+    WHERE change_date <= DATE('2019-08-16')
+    GROUP BY product_id
 )
+
 SELECT 
-    p.product_id,
-    COALESCE(pod.new_price, 10) AS price
-FROM 
-    (SELECT DISTINCT product_id FROM Products) p
-LEFT JOIN 
-    prices_on_date pod
-ON 
-    p.product_id = pod.product_id;
+    ap.product_id, 
+    COALESCE(p.new_price, 10) AS price
+FROM AllProducts AS ap
+LEFT JOIN ProductLastUpdate AS lu
+    ON ap.product_id = lu.product_id 
+LEFT JOIN Products AS p
+    ON ap.product_id = p.product_id AND p.change_date = lu.last_change_date 
