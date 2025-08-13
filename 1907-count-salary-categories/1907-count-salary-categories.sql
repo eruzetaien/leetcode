418b06a0e477167913f441257f62bf6385e569ec
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-08-13 10:33:55 +0700

    Time: 3618 ms (45.48%), Space: 0B (100%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,31 +1,25 @@
-# Write your MySQL query statement below
-WITH SalaryCategory AS (
-    SELECT 
-        CASE 
-            WHEN income < 20000 THEN 'Low Salary'
-            WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
-            WHEN income > 50000 THEN 'High Salary'
-            ELSE '' 
-        END AS category
-    FROM Accounts
-), 
-SalaryCategoryCount AS (
-    SELECT 
-        category,
-        COUNT(*) AS accounts_count
-    FROM SalaryCategory
-    GROUP BY category
-)
-
-SELECT 
-    SC.category,        
-    COALESCE(SCC.accounts_count, 0) AS accounts_count
-FROM (
+-- Write your PostgreSQL query statement below
+WITH categories AS (
     SELECT 'Low Salary' AS category
     UNION ALL
     SELECT 'Average Salary'
     UNION ALL
     SELECT 'High Salary'
-) AS SC 
-LEFT JOIN SalaryCategoryCount AS SCC
-ON SC.category = SCC.category
+),
+counts AS (
+    SELECT
+        CASE
+            WHEN income < 20000 THEN 'Low Salary'
+            WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
+            ELSE 'High Salary'
+        END AS category,
+        COUNT(*) AS accounts_count
+    FROM Accounts
+    GROUP BY category
+)
+SELECT 
+    c.category,
+    COALESCE(cnt.accounts_count, 0) AS accounts_count
+FROM categories c
+LEFT JOIN counts cnt
+ON c.category = cnt.category;
