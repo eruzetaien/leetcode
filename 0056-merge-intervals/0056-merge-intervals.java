d658a257fb9c6929cee40e216b6387993132201b
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-08-16 07:10:09 +0700

    Time: 7 ms (98.63%), Space: 46.9 MB (25.48%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -2,14 +2,14 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         if (intervals.length <= 1) return intervals;
 
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
         List<int[]> merged = new ArrayList<>();
+
         int[] current = intervals[0];
         merged.add(current);
 
-        for (int[] interval : intervals) {
-            if (interval[0] <= current[1]) {
+        for (int[] interval : intervals){
+            if (interval[0] <= current[1]){
                 current[1] = Math.max(current[1], interval[1]);
             } else {
                 current = interval;
