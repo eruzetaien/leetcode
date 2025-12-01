78495d61ed23d691c02ae801f46afe1a229aa6b9
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2025-12-01 20:27:02 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,18 +1,15 @@
-# Write your MySQL query statement below
-SELECT ROUND(SUM(I1.tiv_2016),2) AS tiv_2016
-FROM Insurance AS I1
-WHERE 
-    NOT EXISTS (
-        SELECT I2.pid 
-        FROM Insurance AS I2 
-        WHERE I2.pid != I1.pid AND
-            I1.lat = I2.lat AND
-            I1.lon = I2.lon 
+/* Write your T-SQL query statement below */
+SELECT SUM(tiv_2016) AS tiv_2016 
+FROM Insurance AS I
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM Insurance
+        WHERE I.pid != pid 
+            AND (I.lat = lat AND I.lon = lon) 
+    ) 
+    AND EXISTS (
+        SELECT 1
+        FROM Insurance
+        WHERE I.pid != pid 
+            AND I.tiv_2015 = tiv_2015  
     )
-    AND
-    EXISTS (
-        SELECT I3.pid 
-        FROM Insurance AS I3 
-        WHERE I3.pid != I1.pid AND
-            I3.tiv_2015 = I1.tiv_2015
-    )  
