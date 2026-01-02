b98619b8f822bccdfa25a8d77106a952efe1b45f
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2026-01-02 21:25:59 +0700

    Time: 1033 ms (18.89%), Space: 0B (100%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -1,8 +1,9 @@
-# Write your MySQL query statement below
-SELECT 
-    *
-FROM 
-    Users
-WHERE 
-    mail REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\\.com$'
-    
+/* Write your T-SQL query statement below */
+SELECT user_id, name, mail
+FROM Users
+WHERE mail LIKE '[A-Za-z]%'
+    AND mail COLLATE Latin1_General_BIN LIKE '%@leetcode.com'
+
+  AND LEN(mail) - LEN(REPLACE(mail, '@', '')) = 1
+  AND LEFT(mail, CHARINDEX('@', mail) - 1)
+      NOT LIKE '%[^A-Za-z0-9_.-]%';
