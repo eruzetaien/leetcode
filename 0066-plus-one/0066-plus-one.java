1721762a1bcbd770a89f3e39b7bb87bd3ff9a593
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2026-02-06 21:05:28 +0700

    Time: 0 ms (100%), Space: 43.2 MB (93.46%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,31 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length; 
-        boolean isAllNine = true;
-        for (int i = 0; i < n; i++){
-            if (digits[i] != 9){
-                isAllNine = false;
-                break;
+
+        for (int i = n-1; i >= 0; i--){
+            if (digits[i] < 9){
+                digits[i] += 1;
+                return digits;
             }
+            digits[i] = 0;
         }
 
-        if (isAllNine){
-            int[] result = new int[n + 1];
-            result[0] = 1;
-            return result;
-        }
-
-        int reminder = 1;
-        int pointer = n - 1;
-        while (reminder > 0 && pointer >= 0){
-            digits[pointer] += 1;
-            reminder -= 1;
-            if (digits[pointer] == 10){
-                digits[pointer] = 0;
-                reminder += 1;
-            }
-            pointer -= 1;
-        }
-        return digits;
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
     }
 }
