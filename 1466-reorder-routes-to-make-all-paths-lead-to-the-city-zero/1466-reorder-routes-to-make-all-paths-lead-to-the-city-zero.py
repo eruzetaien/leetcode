aa8cded5f14c91a9ebe310f49a23116f672f8e41
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2026-03-31 20:40:58 +0700

    Time: 309 ms (5.01%), Space: 43.7 MB (89.25%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,21 +1,28 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        for a, b in connections:
-            graph[a].append((b, 1))  
-            graph[b].append((a, 0))  
+        neighbors: List[List[int]] = [[] for _ in range(n)]
         
-        visited = set()
-        changes = 0
+        reorder_edges: Set[int,int] = set()
+        for con in connections:
+            neighbors[con[0]].append(con[1])
+            neighbors[con[1]].append(con[0])
+            reorder_edges.add((con[1],con[0]))
 
-        def dfs(node: int):
-            nonlocal changes
-            visited.add(node)
-            for neighbor, needs_change in graph[node]:
-                if neighbor not in visited:
-                    changes += needs_change
-                    dfs(neighbor)
-        
-        dfs(0)
-        return changes
+        print(neighbors)
 
+        reorder_times: int = 0
+        visited: Set[int] = set()
+        stack: List[int] = [0]
+        while(stack):
+            city: int = stack.pop()
+            visited.add(city)
+
+            for neigh in neighbors[city]:
+                if neigh not in visited:
+                    if (neigh,city) in reorder_edges:
+                        reorder_times += 1
+                    stack.append(neigh)
+
+            print(city, neigh, reorder_times)
+
+        return reorder_times
