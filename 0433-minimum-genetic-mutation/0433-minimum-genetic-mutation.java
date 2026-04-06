f2548b7996d9c4ea5f8600103ee8fdf71b99ae79
Ruzain <103396775+eruzetaien@users.noreply.github.com>
2026-04-06 20:32:49 +0700

    Time: 1 ms (94.78%), Space: 43 MB (22.43%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -1,37 +1,43 @@
-class Solution {
+public class Solution {
     public int minMutation(String startGene, String endGene, String[] bank) {
         Set<String> bankSet = new HashSet<>(Arrays.asList(bank));
-        if (!bankSet.contains(endGene))
+        if (!bankSet.contains(endGene)) 
             return -1;
 
-        char[] geneChoices = new char[] {'A', 'C', 'G', 'T'};
-
         Queue<String> queue = new LinkedList<>();
-        queue.offer(startGene);
+        queue.add(startGene);
 
-        int mutationCount = 0;
-        while (!queue.isEmpty()){
-            int size = queue.size();
-            mutationCount++;
-            for (int i = 0; i < size; i++){
-                String gene = queue.poll();
-                for (int c = 0; c < gene.length(); c++){
-                    char oldChar = gene.charAt(c);
-                    for (char ch : geneChoices){
-                        gene = gene.substring(0, c) + ch + gene.substring(c + 1);
-                        if (gene.equals(endGene))
-                            return mutationCount;
+        Map<String, Integer> visited = new HashMap<>();
+        visited.put(startGene, 0);
 
-                        if (bankSet.contains(gene)){
-                            bankSet.remove(gene);
-                            queue.offer(gene);
-                        } 
-                    }
-                    gene = gene.substring(0, c) + oldChar + gene.substring(c + 1);;
+        while (!queue.isEmpty()) {
+            String gene = queue.poll();
+            int steps = visited.get(gene);
+
+            if (gene.equals(endGene)) {
+                return steps;
+            }
+
+            for (String nextGene : bankSet) {
+                if (!visited.containsKey(nextGene) && isOneMutation(gene, nextGene)) {
+                    visited.put(nextGene, steps + 1);
+                    queue.add(nextGene);
                 }
             }
         }
-        return -1;
 
+        return -1; 
     }
+
+    private boolean isOneMutation(String gene1, String gene2) {
+        int diff = 0;
+        for (int i = 0; i < gene1.length(); i++) {
+            if (gene1.charAt(i) != gene2.charAt(i)) {
+                diff++;
+                if (diff > 1) return false;
+            }
+        }
+        return diff == 1;
+    }
+
 }
